a3a976767e70b6012c34bf465491f01111311390
Guillermo Aguirre <guillermo.aguirre@imanage.com>
2019-06-19 13:54:50 -0500

    Modified return type format and accessibles

--- mtdclient.py
+++ mtdclient.py
@@ -24,13 +24,13 @@ class MTDClient:
 
         for stop_loc in page_json_data["stops"]:
             for stop in stop_loc["stop_points"]:
-                stop_entry = namedtuple('stop_id','stop_name')
+                stop_entry = namedtuple('stop_entry','stop_id stop_name')
                 stop_entry.stop_id = stop["stop_id"]
                 stop_entry.stop_name = stop["stop_name"]
                 stop_entries.append(stop_entry)
         return stop_entries
 
-    def get_departures_by_stop(selfisJson):        
+    def get_departures_by_stop(self,stop_id,isJson):        
         api_method = 'getdeparturesbystop?key='
         base_url = api_endpoint + api_method + self.key
         params = '&stop_id='+ stop_id
@@ -39,6 +39,18 @@ class MTDClient:
         
         if isJson:
             return page_json_data
+
+        departure_entries = []
+
+        for departure in page_json_data["departures"]:
+            depart_entry = namedtuple('depart_entry','stop_id bus expected expected_mins')
+            depart_entry.stop_id = departure["stop_id"]
+            depart_entry.bus = departure["headsign"]
+            depart_entry.expected = departure["expected"]
+            depart_entry.expected_mins = departure["expected_mins"]
+            departure_entries.append(depart_entry)
+        return departure_entries
+
 
     
 
