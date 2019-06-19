19fff9d506dc695d335a2495d5af0dff2d4d31ca
Guillermo Aguirre <guillermo.aguirre@imanage.com>
2019-06-19 13:05:23 -0500

    Able to retrieve json objects, still learning requests

--- mtdclient.py
+++ mtdclient.py
@@ -1,24 +1,31 @@
 import json
 import requests
 import datetime
+import urllib.parse as urlp
 from urllib.request import urlopen
-from Helpers.web import join_url_path
 from collections import namedtuple
 
-api_endpoint = "https://developer.cumtd.com/api/v2.2/json/?key="
+api_endpoint = "https://developer.cumtd.com/api/v2.2/json/"
 
 class MTDClient:
-	def __init__(self, key: str):
-		self.key = key
-		api_endpoint = join_url_path(api_endpoint,self.key)
+    def __init__(self, key):
+        self.key = key  
 
-	def get_stops(self):
-		pass
+    def get_stops(self):
+        api_method = 'getstops?key='
+        base_url = api_endpoint + api_method + self.key
+        params = ''
+        page_json_data = requests.get(base_url+params).json()
+        return page_json_data
 
-	def get_departures_by_stop(self, stop_id: str)
-		pass
+    def get_departures_by_stop(self, stop_id):        
+        api_method = 'getdeparturesbystop?key='
+        base_url = api_endpoint + api_method + self.key
+        params = '&stop_id='+ stop_id
 
-	
+        page_json_data = requests.get(base_url+params).json()
+        return page_json_data
+    
 
 
 
