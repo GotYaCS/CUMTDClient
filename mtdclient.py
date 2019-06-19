ad666511a50f904bccf72634a3fc1b8e3d4bcc5c
Guillermo Aguirre <guillermo.aguirre@imanage.com>
2019-06-19 13:34:06 -0500

    Added getstops method and provided .txt for them to avoid additional calls

--- mtdclient.py
+++ mtdclient.py
@@ -11,20 +11,35 @@ class MTDClient:
     def __init__(self, key):
         self.key = key  
 
-    def get_stops(self):
+    def get_stops(self,isJson):
         api_method = 'getstops?key='
         base_url = api_endpoint + api_method + self.key
         params = ''
         page_json_data = requests.get(base_url+params).json()
-        return page_json_data
+        
+        if isJson:
+            return page_json_data
+        
+        stop_entries = []
 
-    def get_departures_by_stop(self, stop_id):        
+        for stop_loc in page_json_data["stops"]:
+            for stop in stop_loc["stop_points"]:
+                stop_entry = namedtuple('stop_id','stop_name')
+                stop_entry.stop_id = stop["stop_id"]
+                stop_entry.stop_name = stop["stop_name"]
+                stop_entries.append(stop_entry)
+        return stop_entries
+
+    def get_departures_by_stop(selfisJson):        
         api_method = 'getdeparturesbystop?key='
         base_url = api_endpoint + api_method + self.key
         params = '&stop_id='+ stop_id
 
         page_json_data = requests.get(base_url+params).json()
-        return page_json_data
+        
+        if isJson:
+            return page_json_data
+
     
 
 
